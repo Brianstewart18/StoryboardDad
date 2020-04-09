2dfd1ff349405c6b5f8f865773e7545b88c707c8
Brianstewart18 <34485887+Brianstewart18@users.noreply.github.com>
2020-04-09 14:45:21 -0500

    Add files via upload

---
+++ app/models/story.js
@@ -0,0 +1,9 @@
+module.exports = function(sequelize, DataTypes) {
+  var Storyboard = sequelize.define("Storyboard", {
+    characterList: DataTypes.STRING,
+    imageurl: DataTypes.TEXT,
+    story: DataTypes.TEXT,
+    genre: DataTypes.TEXT
+  });
+  return Storyboard;
+};
---
+++ server.js
@@ -0,0 +1,79 @@
+require("dotenv").config();
+var express = require("express");
+var bodyParser = require("body-parser");
+var exphbs = require("express-handlebars");
+var passport = require("passport");
+var session = require("express-session");
+var mysql = require("mysql");
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    port: 8889,
+    database: "storyboardDB"
+  });
+}
+
+var db = require("./app/models");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static("public"));
+
+// For Passport
+app.use(
+  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+
+// Handlebars
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main"
+  })
+);
+app.set("view engine", "handlebars");
+
+//Models
+var models = require("./app/models");
+
+// Routes
+require("./app/routes/apiRoutes")(app);
+require("./app/routes/htmlRoutes")(app);
+require("./app/routes/userRoutes")(app);
+require("./app/config/passport/passport.js")(passport, models.user);
+var authRoute = require("./app/routes/auth.js")(app, passport);
+
+var syncOptions = { force: false };
+
+// If running a test, set syncOptions.force to true
+// clearing the `testdb`
+if (process.env.NODE_ENV === "test") {
+  syncOptions.force = true;
+}
+
+// Starting the server, syncing our models ------------------------------------/
+db.sequelize.sync(syncOptions).then(function() {
+  app.listen(PORT, function() {
+    console.log(
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
+  });
+});
+
+connection.connect();
+module.exports = connection;
+module.exports = app;
